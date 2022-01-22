7f652bdf35c547a6b8707058ffd74e1df86b8b43
Brunna Rocha <brunna.rocha@credihome.com.br>
2022-01-22 19:54:54 -0300

    FEATURE: Adding a table component from material-table and rendering data.

---
+++ src/components/BooksTable/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import MaterialTable from 'material-table'
+
+export default function BooksTable() {
+  const books = [
+    {
+      "id": 240,
+      "book_author": [
+        "Worcester, Joseph Emerson (1784–1865) "
+      ],
+      "book_title": "Ιστορία της Αρχαίας Ελλάδος",
+      "book_publication_year": 1831,
+      "book_publication_country": "Μάλτα",
+      "book_publication_city": "Μελίτη",
+      "book_pages": 60
+    },
+    {
+      "id": 245,
+      "book_author": [
+        "Worcester, Joseph Emerson (1784–1865) "
+      ],
+      "book_title": "Σύνοψις της Ρωμαϊκής Ιστορίας",
+      "book_publication_year": 1831,
+      "book_publication_country": "Μάλτα",
+      "book_publication_city": "Μελίτη",
+      "book_pages": 92
+    },
+    {
+      "id": 2482,
+      "book_author": [
+        "Worcester, Joseph Emerson (1784–1865) "
+      ],
+      "book_title": "Ιστορία της Αρχαίας Ελλάδος, εις χρήσιν των Σχολείων",
+      "book_publication_year": 1831,
+      "book_publication_country": "Μάλτα",
+      "book_publication_city": "Μελίτη",
+      "book_pages": 60
+    }
+  ]
+  
+  return (
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        columns={[
+          { title: 'Book Title', field: 'book_title' },
+          { title: 'Author', field: 'book_author' },
+          { title: 'Pages', field: 'book_pages', type: 'numeric' },
+          { title: 'Publication Year', field: 'book_publication_year', type: 'numeric' },
+          { title: 'Publication Country', field: 'book_publication_country' },
+          { title: 'Publication City', field: 'book_publication_city' }
+        ]}
+        data={books}
+        title=""
+      />
+    </div>
+  )
+}
+
