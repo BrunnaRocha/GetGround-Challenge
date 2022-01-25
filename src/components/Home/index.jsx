5c8371fc18a75872317dd8ca5022c5edaf887ff5
Brunna Rocha <brunna.rocha@credihome.com.br>
2022-01-25 02:13:14 -0300

    FEATURE: Transforming a function component in a class component.

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import BooksTable from "../BooksTable";
-import { useDispatch } from "react-redux";
+import { compose } from "redux";
+import { connect } from "react-redux";
 import { fetchBooks } from './actions';
-import { useParams } from "react-router-dom";
+import { useParams, withRouter } from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
@@ -25,29 +26,64 @@ const Description = styled.h3`
   text-align: center;
 `;
 
+class Home extends Component {
+  constructor(props) {
+    super(props);
 
-export default function Home() {
-  const { page } = useParams();
-  const [itemsPerPage, setItemsPerPage] = useState(5);
-  const dispatch = useDispatch();
+    const { params } = this.props.match;
 
-  useEffect(() => {
-    dispatch(fetchBooks(page, itemsPerPage))
-  }, [page, itemsPerPage]);
+    this.state = {
+      page: params.page,
+      itemsPerPage: 5
+    };
 
-  return (
-    <Container>
-      <Title>GetGround Library</Title>
-      <Description>
-        Search for a registered book from our API:
-      </Description>
-      <BooksTable
-        currentPage={page - 1}
-        onChangeRowsPerPage={setItemsPerPage}
-        rowsPerPage={itemsPerPage}
-      />
-    </Container>
-  );
+    this.setItemsPerPage = this.setItemsPerPage.bind(this);
+    this.getBooksFromApi = this.getBooksFromApi.bind(this);
+  }
+
+  setItemsPerPage(itemsPerPage) {
+    this.setState({ itemsPerPage });
+  }
+
+  getBooksFromApi() {
+    this.props.dispatch(fetchBooks(this.props.match.params.page, this.state.itemsPerPage))
+  }
+
+  componentDidMount() {
+    this.getBooksFromApi()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.itemsPerPage !== this.state.itemsPerPage || prevProps.match.params.page !== this.props.match.params.page)
+      this.getBooksFromApi()
+  }
+  
+  render() {
+    const { page, itemsPerPage } = this.state
+
+    return (
+      <Container>
+        <Title>GetGround Library</Title>
+        <Description>
+          Search for a registered book from our API:
+        </Description>
+        <BooksTable
+          currentPage={page - 1}
+          onChangeRowsPerPage={this.setItemsPerPage}
+          rowsPerPage={itemsPerPage}
+        />
+      </Container>
+    );
+  }
 }
 
+function mapDispatchtoProps(dispatch) {
+  return {
+    dispatch
+  }
+}
 
+export default compose(
+  withRouter,
+  connect(mapDispatchtoProps)
+)(Home);
