923e0c86dbc9f99c6ea5cd54b7d2aaa60ca81ee0
Brunna Rocha <brunna.rocha@credihome.com.br>
2022-01-25 01:42:29 -0300

    FEATURE: Adding redux provider and routes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route path="/:page" component={App} />
+        <Route render={() => <Redirect to="/1" />} />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
